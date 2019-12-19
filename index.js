3005b024a2d02874b80a3aad342be54865b55c54
CountNick <meijer.nick1@gmail.com>
2019-12-19 16:47:58 +0100

    :card_file_box: Data fetch
    Data fetch from tsv file working

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 d3.tsv('./rawData.txt')
-    .then(rawJSON => rawJSON); // console log
+    .then(data => {
+        return data
+    })
+    .then(data => transformData(data))
+
+function transformData(data){
+
+    
+
+    const cleanedObjects = data.map(object => {
+        return{
+            vertrouwen: object.rapportcijfer, 
+            contact: object.Contact_gehad,
+            stadsDeel: object.Stadsdeel,
+            totStand: object.Totstand
+        }
+    })
+    
+}
