704dbedfd709fc657f5e1770face1ad22949138c
CountNick <meijer.nick1@gmail.com>
2019-12-20 10:26:09 +0100

    :poop: Filter yes and no button
    Clicking yes now logs the yesArray and clicking no the noArray

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 d3.tsv('./rawData.txt')
     .then(data => {
+        //return the data
         return data
     })
     .then(data => transformData(data))
+    .then(transformData => nestData(transformData))
 
+//function for data transformation
 function transformData(data){
-
-    
-
+    //make new array with modified objects
     const cleanedObjects = data.map(object => {
+        //only return neccessary pairs 
         return{
-            vertrouwen: object.rapportcijfer, 
+            //rename keys for easier usage
+            vertrouwen: +object.rapportcijfer, 
             contact: object.Contact_gehad,
             stadsDeel: object.Stadsdeel,
             totStand: object.Totstand
         }
     })
+
+    console.log(cleanedObjects)
+    //return the cleaned objects
+    return cleanedObjects   
+}
+
+function nestData(data){
+    //empty array to store each object answered with yes
+    const yesArray = []
+    //empty array to store each object answered with no
+    const noArray = []
+    //selects the ja button from the DOM
+    const yesButton = document.getElementById("ja")
+    //selects the no button from the DOM
+    const noButton = document.getElementById("nee")
     
+    //only values containing yes or no will be used in this program
+    //loop through the array of objects
+    for (let object in data){
+        //if contact value equals ja push to the yes array
+        if (data[object].contact == "Ja"){
+            yesArray.push(data[object])
+        } 
+        //if contact value equals nee push to the no array
+        else{
+            noArray.push(data[object])
+        }
+    }
+    //adds a click event to the yes button
+    yesButton.addEventListener("click", function(){
+        console.log(yesArray)
+    })
+    //adds a click event to the yes button
+    noButton.addEventListener("click", function(){
+        console.log(noArray)
+    })
+
+    //calculates the percentage of the yes array
+    let percentYes = Math.floor((yesArray.length / data.length) *100);
+    //calculates the percentage of the no array
+    let percentNo = Math.floor((noArray.length / data.length) * 100);
+    console.log('Wel contact: ', percentYes)
+    console.log('geen contact: ', percentNo)
+
 }
