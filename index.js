d40e5777193fe20e7aea2a6866eb46b38d0ab1c6
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-01-09 15:47:42 +0100

    add step by step content

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
 d3.tsv('./rawData.txt')
     .then(rawJSON => rawJSON); // console log
+
+const button1 = document.getElementById('button-step1');
+const button2 = document.getElementById('button-step2');
+const button3 = document.getElementById('button-step3');
+const button4 = document.getElementById('button-step4');
+
+const step1 = document.getElementById('step1');
+const step2 = document.getElementById('step2');
+const step3 = document.getElementById('step3');
+const step4 = document.getElementById('step4');
+
+button1.addEventListener('click', function() {
+    step1.classList.replace('hidden', 'visible');
+    step2.classList.replace('visible', 'hidden');
+    step3.classList.replace('visible', 'hidden');
+    step4.classList.replace('visible', 'hidden');
+
+    button1.classList.replace('inactive', 'active');
+    button2.classList.replace('active', 'inactive');
+    button3.classList.replace('active', 'inactive');
+    button4.classList.replace('active', 'inactive');
+});
+
+button2.addEventListener('click', function() {
+    step1.classList.replace('visible', 'hidden');
+    step2.classList.replace('hidden', 'visible');
+    step3.classList.replace('visible', 'hidden');
+    step4.classList.replace('visible', 'hidden');
+
+    button1.classList.replace('active', 'inactive');
+    button2.classList.replace('inactive', 'active');
+    button3.classList.replace('active', 'inactive');
+    button4.classList.replace('active', 'inactive');
+});
+
+button3.addEventListener('click', function() {
+    step1.classList.replace('visible', 'hidden');
+    step2.classList.replace('visible', 'hidden');
+    step3.classList.replace('hidden', 'visible');
+    step4.classList.replace('visible', 'hidden');
+
+    button1.classList.replace('active', 'inactive');
+    button2.classList.replace('active', 'inactive');
+    button3.classList.replace('inactive', 'active');
+    button4.classList.replace('active', 'inactive');
+});
+
+button4.addEventListener('click', function() {
+    step1.classList.replace('visible', 'hidden');
+    step2.classList.replace('visible', 'hidden');
+    step3.classList.replace('visible', 'hidden');
+    step4.classList.replace('hidden', 'visible');
+
+    button1.classList.replace('active', 'inactive');
+    button2.classList.replace('active', 'inactive');
+    button3.classList.replace('active', 'inactive');
+    button4.classList.replace('inactive', 'active');
+});
