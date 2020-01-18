418f69f1d42e43e57aaff64fdc4b77fc7196f3a7
CountNick <meijer.nick1@gmail.com>
2020-01-18 18:31:49 +0100

    :bug: Stacked fill and stroke
    Fixed the stroke and fill on the right sides

--- index.js
+++ index.js
@@ -115,8 +115,8 @@ function splitIntoArrays(data){
     // console.log('Nederlandsz', originNederlands.length / answerYes.length * 100);
     const total = answerNo.length + answerYes.length;
     
-    complete.push(prepareNormalisedStackData(originNietWestersAnswerYes, answerYes));
     complete.push(prepareNormalisedStackData(originNederlandsAnswerYes, answerYes));
+    complete.push(prepareNormalisedStackData(originNietWestersAnswerYes, answerYes));
     complete.push(prepareNormalisedStackData(originWestersAnswerYes, answerYes));
 
     //fill the pieData array with each origin and it's corresponding value in percentage
